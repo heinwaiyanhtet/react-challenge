d805c2f5c86ceff6ec0c7185e9cd5b10af465910
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-06-24 00:24:34 +0630

    test: props

--- src/index.js
+++ src/index.js
@@ -4,38 +4,84 @@ import "./styles.css";
 import App from "./app.js";
 
 
+// const root = createRoot(document.getElementById("root"));
+
+// root.render(
+
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+
+// );
+
+
+
+
+
+
+// const root = createRoot(document.getElementById("root"));
+
+// let h1 = React.createElement('h1', null, 'Hello world!')
+
+// class HelloWorld extends React.Component
+// {
+//   render() {
+//     return React.createElement('div', null, h1, h1)
+//   }
+// }
+
+
+// root.render(
+
+//   React.createElement
+//   (
+//      'div',
+//      null,
+//      React.createElement(HelloWorld),
+//      React.createElement(HelloWorld),
+//      React.createElement(HelloWorld)
+//   ),
+
+//   document.getElementById('content')
+
+// );
+
+
+
+
+
 const root = createRoot(document.getElementById("root"));
 
+class HelloWorld extends React.Component{
 
-let h1 = React.createElement('h1', null, 'Hello world!')
+    render()
+    {
+        return React.createElement(
+            'h1',
+            this.props,
+            'Hello ' + this.props.frameworkName + ' World!!!' 
+        )
+    }
 
-class HelloWorld extends React.Component
-{
-  render() {
-    return React.createElement('div', null, h1, h1)
-  }
 }
-
-
 
 root.render(
 
+    React.createElement(
 
-  React.createElement
-  (
-     'div',
-     null,
-     React.createElement(HelloWorld),
-     React.createElement(HelloWorld),
-     React.createElement(HelloWorld)
-  ),
-
-
-  document.getElementById('content')
-
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>
+        'div',
+         null,
+        React.createElement(HelloWorld,{
+           id : "ember",
+           frameworkName : "Ember.js",
+           title : 'A framework for creating ambitious web applications.'
+        }),
 
 
-);
+        document.getElementById('content')
+
+    )
+
+
+
+)

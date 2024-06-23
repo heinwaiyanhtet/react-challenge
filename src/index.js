8ec7250805d51d9174a7121a0f51c4f38ce6c5b2
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-06-23 23:58:38 +0630

    react-quickly

--- src/index.js
+++ src/index.js
@@ -3,9 +3,31 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./app.js";
 
+
 const root = createRoot(document.getElementById("root"));
+
+let h1 = React.createElement('h1', null, 'Hello world!')
+
+class HelloWorld extends React.Component
+{
+  render() {
+    return React.createElement('div', null, h1, h1)
+  }
+}
+
+
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+
+React.createElement(HelloWorld, null),
+
+
+document.getElementById('content')
+
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>
+
+
 );
